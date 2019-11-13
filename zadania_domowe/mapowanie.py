9f0058e2f11e75ad930bfc1813334ba523eba2d5
dzakub77 <jak.inglot@gmail.com>
2019-11-13 21:11:34 +0100

    Zadania

--- zadania_domowe/mapowanie.py
+++ zadania_domowe/mapowanie.py
@@ -6,9 +6,12 @@ osoby2 = ['marek', 'przemek', "michał", "kamila"]
 random.shuffle(osoby)
 random.shuffle(osoby2)
 licznik = 0
-while licznik != 8:
+while licznik != 4:
     for os1, os2 in zip(osoby, osoby2):
-
-        print(f"{os1} kupuje prezent {os2}")
-        licznik += 1
+        if os1 == os2:
+            print()
+            licznik -= 1
+        else:
+            print(f"{os1} kupuje prezent {os2}")
+            licznik += 1
 
